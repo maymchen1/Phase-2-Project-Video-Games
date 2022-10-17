da9529b6ad8df6ff638d3b7a198e97aae9c9c42d
May Chen <maymchen1@gmail.com>
2022-10-17 16:35:08 -0400

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GameForm from "./components /GameForm";
+import GameList from "./components /GameList";
+import Home from "./components /Home";
+import Navbar from "./components /Navbar";
 
-function App() {
+
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={ <Home />}/>
+        <Route path="/games" element={ <GameList />}/>
+        <Route path="/games/new" element={ <GameForm />}/>
+      </Routes>
+    </Router>
   );
 }
 
