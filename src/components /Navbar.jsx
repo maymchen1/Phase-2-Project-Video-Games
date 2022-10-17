bebc15daf65a5ef01903ffd0f08d6697dcad674f
May Chen <maymchen1@gmail.com>
2022-10-17 16:56:28 -0400

    changes to navbar

--- src/components /Navbar.jsx
+++ src/components /Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        <li><Link to = "/"> Home</Link></li>
+        <li><Link to = "/games"> Games</Link></li>
+        <li><Link to = "/games/new"> Create Game</Link></li>
+      </ul>
+    </nav>>
   )
 }
 
