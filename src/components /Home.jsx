9bc34e6b1a3ef862e453d527a7a2968dd65acbdf
May Chen <maymchen1@gmail.com>
2022-10-17 16:49:53 -0400

    added homepage text

--- src/components /Home.jsx
+++ src/components /Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to your personal video game tracking website.</h1>
+      <p>Use our webpage to track games you are curreuntly playing, add new games to your list and search for games to play!</p>
+        </div>
   )
 }
 
