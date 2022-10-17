6ae203166a7c28c4e476b9358a3b040bbf48f25e
May Chen <maymchen1@gmail.com>
2022-10-17 17:01:16 -0400

    imported link to navbar

--- src/components /Navbar.jsx
+++ src/components /Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to = "/"> Home</Link></li>
-        <li><Link to = "/games"> Games</Link></li>
-        <li><Link to = "/games/new"> Create Game</Link></li>
+        <li><Link to = "/">Home</Link></li>
+        <li><Link to = "/games">Games</Link></li>
+        <li><Link to = "/games/new">Create Game</Link></li>
       </ul>
-    </nav>>
+    </nav>
   )
 }
 
