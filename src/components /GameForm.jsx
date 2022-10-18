c3d25f41ec48d13a083ded6ff59db9c1a31e503b
May Chen <maymchen1@gmail.com>
2022-10-18 02:38:19 -0400

    changes to game form

--- src/components /GameForm.jsx
+++ src/components /GameForm.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const GameForm = () => {
   return (
-    <div>GameForm</div>
+    <div>
+      <h3>Create Game</h3>
+      <form>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input 
+          type="text" 
+          name="name" 
+          id="name"
+          value={ name }
+          onChange={e=>setName(e.target.value)}/>
+        </div>
+        <div>
+        <label htmlFor='genre'>Name:</label>
+          <input 
+          type="text" 
+          name="genre" 
+          id="genre"
+          value={ genre }
+          onChange={e=>setGenre(e.target.value)}/>
+        </div>
+      </form>
+    </div>
   )
 }
 
