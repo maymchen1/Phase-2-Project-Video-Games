5cb33c42b4db02b7bd11a36c4c48628d7578d49e
May Chen <maymchen1@gmail.com>
2022-10-26 21:05:56 -0400

    changes to form

--- src/components /Home.jsx
+++ src/components /Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to your personal video game tracking website.</h1>
-      <p>Use our webpage to track games you are curreuntly playing, add new games to your list and search for games to play!</p>
+      <h1>Welcome to Videogames.gg your personal video game tracking website.</h1>
+      <p>Use our webpage to track games you are curreuntly playing, add new games to your list and search for games to play! Click on a page to begin.</p>
         </div>
   )
 }
