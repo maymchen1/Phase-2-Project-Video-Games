14dc8b05575e1a920b3aebf65925b3f29aaf5078
May Chen <maymchen1@gmail.com>
2022-10-26 21:06:14 -0400

    changes

--- src/components /GameForm.jsx
+++ src/components /GameForm.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GameForm = () => {
+  const [name, setName] = useState ('')
+
   return (
     <div>
-      <h3>Create Game</h3>
+      <h3>Add Game</h3>
       <form>
         <div>
-          <label htmlFor='name'>Name:</label>
+          <label htmlFor='name'>Name: </label>
           <input 
           type="text" 
           name="name" 
@@ -15,13 +17,45 @@ const GameForm = () => {
           onChange={e=>setName(e.target.value)}/>
         </div>
         <div>
-        <label htmlFor='genre'>Name:</label>
+        <label htmlFor='publisher'>Publisher:</label>
+          <input 
+          type="text" 
+          name="publisher" 
+          id="publisher"
+          value={ publisher }
+          onChange={e=>setPublisher(e.target.value)}/>
+        </div>
+        <div>
+        <label htmlFor='genre'>Genre:</label>
           <input 
           type="text" 
           name="genre" 
           id="genre"
           value={ genre }
           onChange={e=>setGenre(e.target.value)}/>
+        </div>
+        <div>
+        <label htmlFor='year'>Release Year:</label>
+          <input 
+          type="text" 
+          name="year" 
+          id="year"
+          value={ year }
+          onChange={e=>setYear(e.target.value)}/>
+        </div>
+        <div>
+        <label htmlFor="status">Play Status</label>
+        <select 
+        name="status" 
+        value= { status }
+        id="status"
+        onChange={ e => setStatus(e.target.value)}
+        >
+          <option value="In-Progress">In-Progress</option>
+          <option value="Completed">Completed</option>
+          <option value="Want to Play">Want to Play</option>
+          <option value="Paused">Paused</option>
+        </select>
         </div>
       </form>
     </div>
